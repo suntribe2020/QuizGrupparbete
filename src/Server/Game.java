311ad0e1bbd91fe04411d747028ca5a4e9e951a3
”Henrik <henrik.elofsson@gmail.com>
2020-11-26 14:04:17 +0100

    changed name of variables to activePlayer and waitingPlayer

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -9,16 +9,16 @@ public class Game extends Thread {
 
     private final int numberOfRounds = Database.getNumberOfRounds();
     private final int numberOfQuestions = Database.getNumberOfQuestions();
-    private Player playerToStart;
-    private Player playerToWait;
+    private Player activePlayer;
+    private Player waitingPlayer;
     private boolean isGameOver;
     private int playedRounds = 0;
 
     public List<Question> currentCategory = new ArrayList<>(4);
 
-    public Game(Player playerToStart, Player playerToWait) {
-        this.playerToStart = playerToStart;
-        this.playerToWait = playerToWait;
+    public Game(Player activePlayer, Player waitingPlayer) {
+        this.activePlayer = activePlayer;
+        this.waitingPlayer = waitingPlayer;
     }
 
     @Override
@@ -44,9 +44,9 @@ public class Game extends Thread {
 
         while (!isValidChoice) {
             if (playedRounds==0) {
-                result = initiateRound(playerToStart, "Please choose a category");
+                result = initiateRound(activePlayer, "Please choose a category");
             } else{
-                result = initiateRound(playerToStart, "Your score this round was: " + playerToStart.getRoundScore() + " Please choose a category");
+                result = initiateRound(activePlayer, "Your score this round was: " + activePlayer.getRoundScore() + " Please choose a category");
             }
             if (result.equalsIgnoreCase("Music")) {
 
@@ -64,24 +64,24 @@ public class Game extends Thread {
                 currentCategory = Database.getSportQuestions();
                 isValidChoice = true;
             } else {
-                initiateRound(playerToStart, "Choose a valid option");
+                initiateRound(activePlayer, "Choose a valid option");
             }
         }
         Collections.shuffle(currentCategory);
 
-        playQuestionRound(playerToStart);
+        playQuestionRound(activePlayer);
 
         //player two round
         String welcomeMessage = "Chosen categories to play was: " + result + " Are you ready to start?";
-        initiateRound(playerToWait, welcomeMessage);
-        playQuestionRound(playerToWait);
+        initiateRound(waitingPlayer, welcomeMessage);
+        playQuestionRound(waitingPlayer);
         flipPlayers();
     }
 
     private void flipPlayers() {
-        Player tempPlayer = playerToWait;
-        playerToWait = playerToStart;
-        playerToStart = tempPlayer;
+        Player tempPlayer = waitingPlayer;
+        waitingPlayer = activePlayer;
+        activePlayer = tempPlayer;
     }
 
     private String initiateRound(Player player, String message) throws IOException {
@@ -109,17 +109,17 @@ public class Game extends Thread {
 
         player.addToTotalScore(player.getRoundScore());
 
-        if(player.equals(playerToWait)){
+        if(player.equals(waitingPlayer)){
             playedRounds++;
             if(playedRounds==1) {
-                player.writeToClient("Score this round: " + player.getRoundScore() + ". Oppenent scored: " + playerToStart.getRoundScore());
+                player.writeToClient("Score this round: " + player.getRoundScore() + ". Oppenent scored: " + activePlayer.getRoundScore());
 //                playerToStart.writeToClient("Your opponent scored:  " + player.getRoundScore());
             } else {
                 player.writeToClient("Score this round: " + player.getRoundScore() + ". Total score:  " + player.getTotalScore());
 //                playerToStart.writeToClient("Your opponent scored:  " + player.getRoundScore() + ". Their total score:  " + player.getTotalScore());
             }
-            playerToStart.setRoundScore(0);
-            playerToWait.setRoundScore(0);
+            activePlayer.setRoundScore(0);
+            waitingPlayer.setRoundScore(0);
         } else {
             if(playedRounds==0) {
                 player.writeToClient("Score this round: " + player.getRoundScore() + ". Waiting for opponent to finish round");
@@ -127,8 +127,8 @@ public class Game extends Thread {
         }
         isGameOver = checkIfGameOver();
         if(isGameOver) {
-            writeEndMessage(playerToStart, playerToWait);
-            writeEndMessage(playerToWait, playerToStart);
+            writeEndMessage(activePlayer, waitingPlayer);
+            writeEndMessage(waitingPlayer, activePlayer);
         }
     }
 
